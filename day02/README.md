cba7f81bf8a00f979b0a59b745d5b72016d01abe
Max Roncace <me@caseif.net>
2023-12-02 23:49:35 -0500

    Add day 02 solution

---
+++ day02/README.md
@@ -0,0 +1,10 @@
+# Day 02
+
+Today's problem was a lot easier, although because I'm still pretty, well, rusty with Rust iterators it ended up taking
+me some time anyway. As always, the code is pretty messy and contains exactly zero error-checking, but hey, that's what
+AoC is all about.
+
+Part B was super easy to implement because it literally just required tracking the maximum value per color per game and
+then adding some math to calculate the part B answer. I was expecting it to be something like the balls are actually
+removed from the bag each game and what's the minimum number of each color necessary for this, but the actual problem
+was just as straightforward or maybe even moreso.
---
+++ day02/day02.rs
@@ -0,0 +1,78 @@
+use std::cmp::max;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+
+const k_red_count: u32 = 12;
+const k_green_count: u32 = 13;
+const k_blue_count: u32 = 14;
+
+fn main() {
+    let (part_a, part_b) = solve();
+    println!("Part A: {}", part_a);
+    println!("Part B: {}", part_b);
+}
+
+fn read_input_file() -> Vec<String> {
+    let file = File::open("./input.txt").unwrap();
+    return io::BufReader::new(file).lines()
+            .filter(Result::is_ok)
+            .map(Result::unwrap)
+            .filter(|s| !s.is_empty())
+            .collect();
+}
+
+fn solve() -> (u32, u32) {
+    let mut part_a = 0;
+    let mut part_b = 0;
+
+    let mut game = 1;
+    for line in read_input_file() {
+        let mut impossible = false;
+
+        let game_sets = line.split(": ").nth(1).unwrap().split("; ")
+                .map(|s| s.split(", ")
+                        .map(|s| s.split(" "))
+                        .map(|mut spl| (spl.next().unwrap(), spl.next().unwrap()))
+                        .collect::<Vec<(&str, &str)>>())
+                .collect::<Vec<Vec<(&str, &str)>>>();
+
+        let mut max_red = 0;
+        let mut max_green = 0;
+        let mut max_blue = 0;
+
+        for set in game_sets {
+            let mut red = 0;
+            let mut green = 0;
+            let mut blue = 0;
+
+            for (val, color) in set {
+                let num = str::parse::<u32>(val).unwrap();
+                match color {
+                    "red" => red += num,
+                    "green" => green += num,
+                    "blue" => blue += num,
+                    _ => panic!(),
+                };
+            }
+
+            if red > k_red_count || green > k_green_count || blue > k_blue_count {
+                impossible = true;
+            }
+
+            max_red = max(max_red, red);
+            max_green = max(max_green, green);
+            max_blue = max(max_blue, blue);
+        }
+
+        if !impossible {
+            part_a += game;
+        }
+
+        part_b += max_red * max_green * max_blue;
+
+        game += 1;
+    }
+
+    return (part_a, part_b);
+}
